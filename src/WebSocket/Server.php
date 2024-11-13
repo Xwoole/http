576de21219881721112736c67cf79e1a8b8db9ba
mohachi <hachiassali15@gmail.com>
2024-11-13 18:00:00 +0100

    feat: add websocket server

---
+++ src/WebSocket/Server.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Xwoole\Http\WebSocket;
+
+use OpenSwoole\WebSocket\Server as WebSocketServer;
+use Xwoole\Http\Functionality\Server as FunctionalityServer;
+
+class Server extends WebSocketServer
+{
+    use FunctionalityServer;
+    
+    
+    
+}
